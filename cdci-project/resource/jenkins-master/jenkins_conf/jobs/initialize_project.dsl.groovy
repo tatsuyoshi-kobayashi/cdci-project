f07f68603ed1271170feadb57d52b3e9ef87311b
Kobayashi <kobayashi.tatsuyoshi.work@gmail.com>
2023-01-31 15:39:41 +0900

    命名の修正

--- cdci-project/resource/jenkins-master/jenkins_conf/jobs/initialize_project.dsl.groovy
+++ cdci-project/resource/jenkins-master/jenkins_conf/jobs/initialize_project.dsl.groovy
@@ -18,8 +18,8 @@ pipelineJob('/initialize_project') {
     COPY_PROJECT_TEMPLATE_JOB: '/aws/copy_project_template',
     CREATE_UNITY_PROJECT_JOB: '/unity/create_unity_project',
     GIT_USER_NAME: 'jenkins',
-    SCM_SCRIPT_PATH: 'jenkins/Jenkinsfile'
-    GIT_USER_EMAIL: JENKINS_URL
+    GIT_USER_EMAIL: JENKINS_URL,
+    SCM_SCRIPT_PATH: 'jenkins/Jenkinsfile',
   )
   definition {
     cps {
