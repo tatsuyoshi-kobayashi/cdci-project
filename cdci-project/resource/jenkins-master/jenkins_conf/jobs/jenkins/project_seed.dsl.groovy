c6e1231ddf37b495aef96b3414f6d0a9303f7f1e
Kobayashi <kobayashi.tatsuyoshi.work@gmail.com>
2023-01-31 14:17:32 +0900

    TABインデント修正

--- cdci-project/resource/jenkins-master/jenkins_conf/jobs/jenkins/project_seed.dsl.groovy
+++ cdci-project/resource/jenkins-master/jenkins_conf/jobs/jenkins/project_seed.dsl.groovy
@@ -2,17 +2,17 @@ String desc = 'プロジェクトのビルドジョブを生成します。'
 
 pipelineJob('/jenkins/project_seed') {
   description(desc)
-	authorization { permission('hudson.model.Item.Workspace:authenticated') }
+  authorization { permission('hudson.model.Item.Workspace:authenticated') }
   parameters {
-		stringParam('GITHUB_REPOSITORY')
-		stringParam('PROJECT_NAME')
-		stringParam('UNITY_PROJECT_NAME')
+    stringParam('GITHUB_REPOSITORY')
+    stringParam('PROJECT_NAME')
+    stringParam('UNITY_PROJECT_NAME')
     stringParam('UNITY_VERSION')
     stringParam('UNITY_HOTFIX_VERSION')
   }
   environmentVariables(
-		PROJECT_JOB_DSL_TEMPLATE: '/var/jenkins_conf/template/project_job.dsl.groovy.template',
-		S3BUCKET_DEPLOY: S3BUCKET_DEPLOY,
+    PROJECT_JOB_DSL_TEMPLATE: '/var/jenkins_conf/template/project_job.dsl.groovy.template',
+    S3BUCKET_DEPLOY: S3BUCKET_DEPLOY,
   )
   definition {
     cps {
